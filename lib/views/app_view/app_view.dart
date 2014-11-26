a25551bb6fba1845c9988b63761d56a5b8c10027
Kishaihd <mr.pinstripe_Suit@hotmail.com>
2014-11-26 12:52:24 -0700

    UI construction.
    Just getting some UI stuff in place, then I will connect things together
    to make them interactive AND useful!

--- lib/views/app_view/app_view.dart
+++ lib/views/app_view/app_view.dart
@@ -9,11 +9,29 @@ import '../../utils/filters.dart';
 @CustomTag('app-view')
 class AppView extends PolymerElement {
 
+  static const String INTRO_VIEW = "INTRO_VIEW";
+  static const String ABILITIES_VIEW = "ABILITIES_VIEW";
+  static const String RACE_VIEW = "RACE_VIEW";
+  static const String CLASS_VIEW = "CLASS_VIEW";
+  
   // initialize system log
   bool _logInitialized = initLog();
 
   @observable String bindingTest = "Binding is working...";
-
+  @observable String bindingTest_abilities = "Abilities. Sup.";
+  @observable String bindingTest_intro = "Main page. Sup.";
+  @observable bool abilitiesDone = false;
+  @observable bool raceDone = false;
+  @observable bool classDone = false;
+  @observable bool characterCreated = false;
+  
+  @observable String currentView = INTRO_VIEW;
+  @observable int tabSelected = 0;
+  
+  void changeView(Event e, var detail, Element target) {
+        currentView = target.attributes['data-view'];
+    }
+  
   // filters and transformers can be referenced as class fields
   final Transformer asInteger = new StringToInt();
 
